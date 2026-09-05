dff7c68a753946e70fd3c092bc90d7b23da2c58c
Darchiashvili9 <gdarchiashvili@hotmail.com>
2026-09-05 15:59:49 +0400

    Add product concurrency handling and merge duplicate order items

--- OrderFlow.Api/Services/OrderService.cs
+++ OrderFlow.Api/Services/OrderService.cs
@@ -24,22 +24,56 @@ namespace OrderFlow.Api.Services
 
             Order order = new(customer);
 
-            var productIds = list.Select(item => item.ProductId).Distinct().ToList();
+            var groupedList = list
+                .GroupBy(p => p.ProductId)
+                .Select(group => new CreateOrderItemDto
+                {
+                    ProductId = group.Key,
+                    Quantity = group.Sum(q => q.Quantity)
+
+                }).ToList();
+
+            var productIds = groupedList.Select(item => item.ProductId).Distinct().ToList();
 
             var products = await _context.Products.Where(prod => productIds.Contains(prod.Id)).ToListAsync();
 
             if (productIds.Count != products.Count)
                 throw new NotFoundException("One or more products were not found");
 
-            foreach (var item in list)
+            foreach (var item in groupedList)
             {
                 var product = products.Single(prod => prod.Id == item.ProductId);
                 order.AddItem(product, item.Quantity);
             }
 
             _context.Orders.Add(order);
-            await _context.SaveChangesAsync();
-            return order;
+
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return order;
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (i == 2)
+                        throw;
+
+                    //reload products
+                    foreach (var product in products)
+                    {
+                        await _context.Entry(product).ReloadAsync();
+                    }
+
+                    foreach (var item in groupedList)
+                    {
+                        var product = products.Single(prod => prod.Id == item.ProductId);
+                        product.ReduceStock(item.Quantity);
+                    }
+                }
+            }
+            throw new InvalidOperationException("Concurrency retry loop exited unexpectedly.");
         }
 
         public async Task<Order?> GetByIdAsync(int orderId)
