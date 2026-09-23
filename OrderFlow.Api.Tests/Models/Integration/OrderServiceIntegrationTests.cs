4fd35466814c1b1782425bc3086cd42c724137c3
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-09-23 19:38:19 +0400

    Expand OrderService integration tests

--- OrderFlow.Api.Tests/Models/Integration/OrderServiceIntegrationTests.cs
+++ OrderFlow.Api.Tests/Models/Integration/OrderServiceIntegrationTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OrderFlow.Api.Data;
 using OrderFlow.Api.DTOs;
 using OrderFlow.Api.Exceptions;
@@ -79,6 +80,149 @@ namespace OrderFlow.Api.Tests.Models.Integration
             using var context = CreateContext();
             OrderService service = new(context);
             await Assert.ThrowsAsync<NotFoundException>(async () => await service.CreateAsync(99999, []));
+        }
+
+        [Fact]
+        public async Task CreateAsync_ProductDoesNotExist_ThrowsNotFoundException()
+        {
+            int custId;
+            {
+                using var context = CreateContext();
+                Customer cust = new("testName", "Deutschland", "testMail@mail.de");
+                context.Customers.Add(cust);
+                await context.SaveChangesAsync();
+                custId = cust.Id;
+            }
+
+            {
+                using var actingContext = CreateContext();
+                OrderService service = new(actingContext);
+                await Assert.ThrowsAsync<NotFoundException>(async () => await service.CreateAsync(custId, [new CreateOrderItemDto() { Quantity = 3, ProductId = 9999 }]));
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_InsufficientStock_ThrowsDomainException()
+        {
+            int custId;
+            int prodId;
+            {
+                using var context = CreateContext();
+                Customer cust = new("testName", "Deutschland", "testMail@mail.de");
+                Product prod = new("testProduct", 2, 5);
+                context.Customers.Add(cust);
+                context.Products.Add(prod);
+                await context.SaveChangesAsync();
+                custId = cust.Id;
+                prodId = prod.Id;
+            }
+
+            {
+                using var actingContext = CreateContext();
+                OrderService service = new(actingContext);
+                await Assert.ThrowsAsync<DomainException>(
+                    async () => await service.CreateAsync(
+                        custId,
+                        [new CreateOrderItemDto() { Quantity = 3, ProductId = prodId }]
+                    )
+                );
+            }
+
+            {
+                using var persistedContext = CreateContext();
+                var product = await persistedContext.Products.SingleAsync(p => p.Id == prodId);
+                Assert.Equal(2, product.Stock);
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_DuplicateProductIds_GroupsQuantities()
+        {
+            int custId;
+            int prodId;
+            int orderId;
+            {
+                using var context = CreateContext();
+                Customer cust = new("testName", "Deutschland", "testMail@mail.de");
+                Product prod = new("testProduct", 30, 50);
+                context.Customers.Add(cust);
+                context.Products.Add(prod);
+                await context.SaveChangesAsync();
+                custId = cust.Id;
+                prodId = prod.Id;
+            }
+
+            {
+                using var actingContext = CreateContext();
+                OrderService service = new(actingContext);
+
+                List<CreateOrderItemDto> ord = new()
+                {
+                    new CreateOrderItemDto() {ProductId = prodId, Quantity = 5},
+                    new CreateOrderItemDto() { ProductId = prodId, Quantity = 10 }
+                };
+                var order = await service.CreateAsync(custId, ord);
+                orderId = order.Id;
+            }
+
+            {
+                using var verificationContext = CreateContext();
+                var product = await verificationContext.Products.SingleAsync(p => p.Id == prodId);
+                var orderItems = await verificationContext.OrderItems.Where(ord => ord.OrderId == orderId).ToListAsync();
+
+                Assert.Equal(15, product.Stock);
+                Assert.Single(orderItems);
+                Assert.Equal(15, orderItems[0].Quantity);
+            }
+        }
+
+        [Fact]
+        public async Task CompleteAsync_PendingOrder_SetsStatusToDone()
+        {
+            int custId;
+            int prodId;
+            int orderId;
+            {
+                using var context = CreateContext();
+                Customer cust = new("testName", "Deutschland", "testMail@mail.de");
+                Product prod = new("testProduct", 30, 50);
+                context.Customers.Add(cust);
+                context.Products.Add(prod);
+                await context.SaveChangesAsync();
+                custId = cust.Id;
+                prodId = prod.Id;
+            }
+
+            {
+                using var actingContext = CreateContext();
+                OrderService service = new(actingContext);
+
+                List<CreateOrderItemDto> ord = new()
+                {
+                    new CreateOrderItemDto() {ProductId = prodId, Quantity = 5},
+                };
+                var order = await service.CreateAsync(custId, ord);
+                orderId = order.Id;
+            }
+
+            {
+                using var completeContext = CreateContext();
+                OrderService service = new(completeContext);
+                await service.CompleteAsync(orderId);
+            }
+
+            {
+                using var verificationContext = CreateContext();
+                var order = await verificationContext.Orders.SingleAsync(o => o.Id == orderId);
+                Assert.Equal(OrderStatus.Done, order.Status);
+            }
+        }
+
+        [Fact]
+        public async Task CancelAsync_PendingOrder_RestoresStockAndSetsStatusToCanceled()
+        {
+
+
         }
     }
 }
